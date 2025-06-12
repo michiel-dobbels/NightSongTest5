8e0b5d0fe43cb06ad1560ae3a04de328696ffa66
michiel-dobbels <michieldobbels97@gmail.com>
2025-06-12 12:49:15 +0200

    Move add buttons above bottom nav

--- app/TopTabsNavigator.tsx
+++ app/TopTabsNavigator.tsx
@@ -44,6 +44,7 @@ const TAB_BAR_HEIGHT = 48;
 const HEADER_BOTTOM_PADDING = 0;
 const BLUR_BACKGROUND_COLOR = 'rgba(29,21,43,0.6)';
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const { height } = Dimensions.get('window');
 const DRAWER_WIDTH = SCREEN_WIDTH * 0.8;
 
 
@@ -326,7 +327,7 @@ const styles = StyleSheet.create({
   },
   fab: {
     position: 'absolute',
-    bottom: 20,
+    bottom: height * 0.15,
     right: 20,
     backgroundColor: '#7814db',
     width: 56,
