70ae1e78f5ceaf199428c1f81720b65b50284d8d
Michiel Dobbels <michieldobbels97@gmail.com>
2025-06-13 18:54:08 +0200

    back to basic listings

---
+++ app/screens/navigation.ts
@@ -0,0 +1,11 @@
+export type RootStackParamList = {
+  MarketHome: {
+    placeholderListing: {
+      id: string;
+      title: string;
+      price: number;
+      isPlaceholder: boolean;
+    };
+  };
+  // Add other screens here as needed
+};
