6474c92c6b3cf807c570b11de8116fdd2870e117
Michiel Dobbels <michieldobbels97@gmail.com>
2025-06-22 08:59:54 +0200

    before stories

---
+++ bottomtabs/MarketScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MarketHomeScreen from '../app/screens/MarketHomeScreen';
+import MarketListingDetailScreen from '../app/screens/MarketListingDetailScreen';
+import CreateListingScreen from '../app/screens/CreateListingScreen';
+import EditListingScreen from '../app/screens/EditListingScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function MarketScreen() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="MarketHome" component={MarketHomeScreen} />
+      <Stack.Screen name="ListingDetail" component={MarketListingDetailScreen} />
+      <Stack.Screen name="CreateListing" component={CreateListingScreen} />
+      <Stack.Screen name="EditListing" component={EditListingScreen} />
+    </Stack.Navigator>
+  );
+}
