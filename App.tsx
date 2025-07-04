4fd2b663f1deadbf2ae90d41c920d8e5e9861f57
michiel-dobbels <michieldobbels97@gmail.com>
2025-07-04 14:25:08 +0200

    Add like notifications

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from './AuthContext';
 import Navigator from './Navigator';
 import { PostStoreProvider } from './app/contexts/PostStoreContext';
 import { StoryStoreProvider } from './app/contexts/StoryStoreContext';
+import { NotificationProvider } from './app/contexts/NotificationContext';
 
 import { Buffer } from 'buffer';
 import process from 'process';
@@ -16,9 +17,11 @@ export default function App() {
     <AuthProvider>
       <PostStoreProvider>
         <StoryStoreProvider>
-          <NavigationContainer>
-            <Navigator />
-          </NavigationContainer>
+          <NotificationProvider>
+            <NavigationContainer>
+              <Navigator />
+            </NavigationContainer>
+          </NotificationProvider>
         </StoryStoreProvider>
       </PostStoreProvider>
     </AuthProvider>
